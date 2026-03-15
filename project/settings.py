93cad86f404d057f9513da45ccdef5288af2a3e0
Anniymm <animaisuradze1999@gmail.com>
2026-03-15 21:58:02 +0400

    final

--- project/settings.py
+++ project/settings.py
@@ -110,3 +110,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os 
+# smtp 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587 #TLS-587, SSL 465
+
+EMAIL_HOST_USER="animaisuradze@gmail.com"
+EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
+
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
